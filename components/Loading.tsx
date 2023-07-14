95a4696106435779be37f2ada21e5228f07862f2
c0ndi <kowalik0konrad@gmail.com>
2023-07-14 14:41:43 +0200

    fix

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className={"h-screen bg-black flex justify-center items-center"}>
       <div className={"text-white"}>
-        <p className={"text-[40px] font-light"}>
+        <p className={"text-[32px] sm:text-[40px] font-light"}>
           AIRBAGS<span className={"text-primary font-semibold"}>PLUS</span>
         </p>
         <div className={"animation h-[2px] bg-primary"}/>
