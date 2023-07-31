126a87d804d0b422118b83bd4ea8e08de7038a2f
c0ndi <kowalik0konrad@gmail.com>
2023-07-31 15:19:40 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,15 @@ import Image from "next/image";
 import GradientOne from "@/public/images/gradient-1.png";
 import GradientTwo from "@/public/images/gradient-2.png";
 import ButtonFixed from "@/components/ButtonFixed";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Loading from "@/components/Loading";
 import Head from "next/head";
+
+const data = {
+  '@context': 'https://schema.org',
+  title: 'AIRBAG PLUS',
+  description: 'Regeneracja poduszek powietrznych.',
+};
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -24,41 +30,47 @@ export default function Home() {
   }, [])
 
   if (isLoading) {
-    return <Loading/>
+    return <Loading />
   }
 
   return (
     <>
       <Head>
         <title>Airbag Plus</title>
-        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"/>
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
         <meta
           name="description"
           content={"Profesjonalna regeneracja konsoli. Naprawiamy deski rozdzielcze po wybuchu poduszki do wszystkich aut osobowych, bez względu na rok produkcji i pochodzenie. Oklejamy nową skają o takiej samej lub maksymalnie zbliżonej strukturze jak w pierwotnej wersji."}
         />
-        <meta property="og:locale" content="pl_PL"/>
-        <meta property="og:site_name" content="AIRBAG PLUS"/>
+        <meta property="og:locale" content="pl_PL" />
+        <meta property="og:site_name" content="AIRBAG PLUS" />
+        <script
+          key="structured-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+        />
+
       </Head>
       <main className={"relative"}>
-        <ButtonFixed/>
+        <ButtonFixed />
 
-        <Navbar/>
-        <Hero/>
+        <Navbar />
+        <Hero />
         <Image
           src={GradientOne}
           alt={""}
           className={"absolute right-0 -z-10"}
         />
         <div className={"flex mx-auto max-w-[1280px] px-2 flex-col"}>
-          <Steps/>
-          <Services/>
-          <Contact/>
+          <Steps />
+          <Services />
+          <Contact />
           <Image
             src={GradientTwo}
             alt={""}
             className={"absolute left-0 bottom-0 -z-10"}
           />
-          <Footer/>
+          <Footer />
         </div>
       </main>
     </>
